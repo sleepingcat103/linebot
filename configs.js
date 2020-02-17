592c5c835f6647eaa64a7154f407edd86ac70990
sleepingcat103 <sleepingcat103@gmail.com>
2020-02-17 22:06:33 +0800

    make it better

--- configs.js
+++ configs.js
@@ -1,12 +1,13 @@
 module.exports = {
-	"environment": "develop",
+	"environment": "product",
 
 	"develop": {
         "lineApi": "https://api.line.me",
-        "lineToken": ""
+        "lineToken": "roU2qoSyY5eXCbE3i+NB7kKS+By3Pff/IGtPlXrXuxEgTiyMQOZGYFGNGSEiwCq1a1VUJYlK5S02hD4H0SqLJ0840NsarVWIrASl8VAx5szdnvx0j/h/bMWvtJebaV5in0Zwt2O7wNRkJQ8Gi1YVSAdB04t89/1O/w1cDnyilFU="
     },
     
 	"product": {
-
+        "lineApi": "https://api.line.me",
+        "lineToken": "fHACwQBpF8Jz2Tvr11NcdBkBAPLftsw+/Nym37Lzux87Sim/mjlBXZ+Uox3wdTMn8unRALSm3SHP3TbjWd+aCFsFioFGkhM4yvzgQnD6fBsFd0s7ANMzGyxhqjRBS549Jw9FUGl5UJVHralGlzbGLAdB04t89/1O/w1cDnyilFU="
     }
 }
