5fa192de13d77c7d459a4880c7b831f64559ebd4
sleepingcat103 <sleepingcat103@gmail.com>
2020-02-17 21:32:11 +0800

    hide token

--- configs.js
+++ configs.js
@@ -3,8 +3,7 @@ module.exports = {
 
 	"develop": {
         "lineApi": "https://api.line.me",
-        "lineToken": "roU2qoSyY5eXCbE3i+NB7kKS+By3Pff/IGtPlXrXuxEgTiyMQOZGYFGNGSEiwCq1a1VUJYlK5S02hD4H0SqLJ0840NsarVWIrASl8VAx5szdnvx0j/h/bMWvtJebaV5in0Zwt2O7wNRkJQ8Gi1YVSAdB04t89/1O/w1cDnyilFU="
-        // "lineToken": "fHACwQBpF8Jz2Tvr11NcdBkBAPLftsw+/Nym37Lzux87Sim/mjlBXZ+Uox3wdTMn8unRALSm3SHP3TbjWd+aCFsFioFGkhM4yvzgQnD6fBsFd0s7ANMzGyxhqjRBS549Jw9FUGl5UJVHralGlzbGLAdB04t89/1O/w1cDnyilFU="
+        "lineToken": ""
     },
     
 	"product": {
