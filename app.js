a0f3584bff532dd0516b64695b163cfd082dc41d
sleepingcat Fang <sleepingcat103@gmail.com>
2022-11-25 16:44:46 +0800

    更新一點點就好

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/resource', express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/line', lineRouter);
 
 // catch 404 and forward to error handler
